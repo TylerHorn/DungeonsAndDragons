d267076850c1107649f18b410148c1be257a77a3
kmnicholas <kmnicholas@dmacc.edu>
2017-02-22 00:13:08 -0600

    Add files via upload

---
+++ FeaturesAndTraits.h
@@ -0,0 +1,23 @@
+#pragma once
+#ifndef FEATURESANDTRAITS_H
+#define FEATURESANDTRAITS_H
+#include<string>
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class FeaturesAndTraits {
+private:
+	vector<string> features;
+public:
+	void setFeatures();
+	void displayFeatures();
+};
+
+void FeaturesAndTraits::setFeatures() {
+	string newFeature;
+	cout << "Enter the feature or trait: \n";
+	cin >> newFeature;
+	features.push_back(newFeature);
+}
+#endif
